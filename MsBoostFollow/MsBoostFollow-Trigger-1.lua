aed0ae13876d7c4c7cc51b17269680c2bb84ec8e
Sander Vispoel <sandervispoel@hotmail.com>
2020-10-20 20:51:46 +0200

    Create MsBoostFollow-Trigger-1.lua

---
+++ MsBoostFollow/MsBoostFollow-Trigger-1.lua
@@ -0,0 +1,28 @@
+-- PLAYER_ENTERING_WORLD,PARTY_INVITE_REQUEST,GROUP_JOINED
+
+function(event, ...)
+    if event == "PLAYER_ENTERING_WORLD" and UnitName("player") == "Msone" then
+        InviteUnit("Mstwo")
+        InviteUnit("Msthree")
+    end
+    
+    -- Accept party invite
+    if event == "PARTY_INVITE_REQUEST" and (UnitName("player") == "Mstwo" or UnitName("player") == "Msthree") then
+        AcceptGroup()
+        StaticPopup_Hide("PARTY_INVITE")
+    end
+    
+    if event == "GROUP_JOINED" then
+        for i = 1,3 do
+            local unit = "party" .. i
+            
+            if UnitIsGroupLeader(unit) then
+                FollowUnit(unit)
+                
+                if IsInGroup() and not UnitIsGroupLeader("player") then
+                    LeaveParty()
+                end
+            end
+        end
+    end
+end
