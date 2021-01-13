068c92e4e59f9ca8bb8d091e818f8b3ea45ad5ba
Sander Vispoel <sandervispoel@hotmail.com>
2021-01-13 17:43:35 +0100

    Fix saved reset

--- NaxxramasTrashChecklist/Checklist-Actions-Init.lua
+++ NaxxramasTrashChecklist/Checklist-Actions-Init.lua
@@ -6,6 +6,7 @@ if aura_env.unitKills == nil then
 
     for i,unit in ipairs(aura_env.config.units) do
         aura_env.unitKills[unit.name] = 0
+        WeakAurasSaved["displays"][aura_env.id].unitKills[unit.name] = 0
     end
 end
 
