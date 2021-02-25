c515be414e1a8041f77726f322d895864b6f7603
Sander Vispoel <sandervispoel@hotmail.com>
2021-02-25 17:16:28 +0100

    Add check for when expiration time is longer than pot cd

--- AoETauntCooldowns/AoETauntCooldowns-Display.lua
+++ AoETauntCooldowns/AoETauntCooldowns-Display.lua
@@ -4,28 +4,32 @@ function()
     local now = GetTime()
     local str = ""
 
-    if aura_env.state.expirationTimeSpell ~= nil and aura_env.state.expirationTimeSpell > now then
-        local delta = aura_env.state.expirationTimeSpell - now
-        local noSeconds = delta > 60
-        local time = SecondsToTime(delta, noSeconds)
+    if
+        aura_env.state.expirationTimeSpell ~= nil
+        and aura_env.state.expirationTimeSpell > now
+        and aura_env.state.expirationTimeSpell - now <= aura_env.spellCd
+    then
+        local dt = aura_env.state.expirationTimeSpell - now
+        local noSeconds = dt > 60
+        local time = SecondsToTime(dt, noSeconds)
 
         str = time
     else
         str = "|cFF03fc03Ready|r"
     end
 
-    if aura_env.state.expirationTimePot ~= nil and aura_env.state.expirationTimePot > now then
-        local delta = aura_env.state.expirationTimePot - now
-        local noSeconds = delta > 60
-        local time = SecondsToTime(delta, noSeconds)
+    if
+        aura_env.state.expirationTimePot ~= nil
+        and aura_env.state.expirationTimePot > now
+        and aura_env.state.expirationTimePot - now <= aura_env.potCd
+    then
+        local dt = aura_env.state.expirationTimePot - now
+        local noSeconds = dt > 60
+        local time = SecondsToTime(dt, noSeconds)
 
         str = str .. " / |cFFeaf522" .. time .. "|r"
     else
         str = str .. " / |cFFeaf522Ready|r"
-    end
-
-    if (aura_env.state.expirationTimeSpell == nil or aura_env.state.expirationTimePot == nil) or now > aura_env.state.expirationTimeSpell and now > aura_env.state.expirationTimePot then
-        str = "|cFF03fc03Ready|r / |cFFeaf522Ready|r"
     end
 
     return str
