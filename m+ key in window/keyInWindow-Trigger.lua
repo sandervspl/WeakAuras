9616056fd48ea14b98ef406ad7936f6115041280
Sander Vispoel <sandervispoel@hotmail.com>
2021-10-06 10:44:46 +0200

    Fix to show tooltip even if char has no key

--- m+ key in window/keyInWindow-Trigger.lua
+++ m+ key in window/keyInWindow-Trigger.lua
@@ -1,4 +1,13 @@
 function(allStates, event, ...)
+    local tooltip = {"Your keys\n"}
+
+    for guid, key in pairs(aura_env.keys) do
+        local str = string.format("%s|r\n|c00FFFFFF%s (|c%s%d|r)\n", key.char_name, key.name, aura_env.getKeyColor(key.lvl), key.lvl)
+        table.insert(tooltip, str)
+    end
+
+    allStates[1] = {}
+
     for bag = 0, NUM_BAG_SLOTS do
         local bagSlots = GetContainerNumSlots(bag)
         for slot = 1, bagSlots do
@@ -20,26 +29,18 @@ function(allStates, event, ...)
                 if numStartIdx and numEndIdx then
                     lvl = string.sub(itemLink, numStartIdx + 1, numEndIdx - 1)
                 end
-
-                local tooltip = {"Your keys\n"}
-
-                for guid, key in pairs(aura_env.keys) do
-                    local str = string.format("%s|r\n|c00FFFFFF%s (|c%s%d|r)\n", key.char_name, key.name, aura_env.getKeyColor(key.lvl), key.lvl)
-                    table.insert(tooltip, str)
-                end
                 
-                allStates[1] = {
-                    changed = true,
-                    show = true,
-                    key_name = name,
-                    key_lvl = lvl,
-                    tooltip = table.concat(tooltip, "\n"),
-                }
+                allStates[1].key_name = name
+                allStates[1].key_lvl = lvl
                 
                 aura_env.update(name, lvl)
             end
         end
     end
+
+    allStates[1].changed = true
+    allStates[1].show = true
+    allStates[1].tooltip = table.concat(tooltip, "\n")
 
     return true
 end
